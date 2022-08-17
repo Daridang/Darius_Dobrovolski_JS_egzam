bcf89884c347d8062a8b16294a95bab73f423b7b
Daridang <daridang@gmail.com>
2022-08-17 22:02:56 +0300

    click me counter done...

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,19 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+// #region target html elements
+const btnStateEl = document.getElementById('btn__state');
+const btnEl = document.querySelector('.btn');
+// #endregion target html elements
+
+// #region init
+let counter = 0;
+// #endregion init
+
+// #region listeners
+btnEl.addEventListener('click', () => {
+    counter++;
+    btnStateEl.innerText = counter;
+})
+// #endregion listeners
