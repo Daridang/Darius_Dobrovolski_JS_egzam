442358f12034b120cc86487ffa1ab94004c6456f
Daridang <daridang@gmail.com>
2022-08-17 23:34:32 +0300

    function filtering users who is a dog owner.

--- Task 5/script.js
+++ Task 5/script.js
@@ -19,3 +19,10 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+// #region task 5.1
+function filterDogOwers(arr) {
+  return arr.filter(user => user.hasDog);
+}
+console.log(filterDogOwers(users));
+// #endregion task 5.1
