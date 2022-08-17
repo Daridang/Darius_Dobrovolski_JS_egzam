06c04c5727f109613878cf544e875f53afec1ed8
Daridang <daridang@gmail.com>
2022-08-17 23:26:44 +0300

    displaying car list from file with some simple css

--- Task 4/script.js
+++ Task 4/script.js
@@ -11,11 +11,38 @@ turėti bent minimalų stilių ir būti responsive;
 
 const ENDPOINT = 'cars.json';
 
+// #region target html elements
+const outputEl = document.getElementById('output');
+// #region target html elements
+
+// #region init
+getCars();
+// #endregion init
+
 // #region fetching data from file
 async function getCars() {
     const response = await fetch('cars.json');
     const result = await response.json();
-    console.log(result);
+    displayData(result)
 }
-getCars();
 // #endregion fetching data from file
+
+// #region helpers
+function displayData(dataArr) {
+    outputEl.innerHTML = '';
+    dataArr.forEach(element => {
+        outputEl.insertAdjacentHTML('afterbegin', createCard(element))
+    });
+}
+// #endregion helpers
+
+// #region card
+function createCard(data) {
+    return `
+    <div class="card">
+        <h2 class="card__title">${data.brand}</h2>
+        <p class="card__body">${data.models.join(', ')}</p>
+    </div>
+    `
+}
+// #endregion card
